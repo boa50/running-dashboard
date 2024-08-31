012838b5e9b2a514deedfda0edbf8941f79cbb02
boa50 <bruno.albuquerque50@gmail.com>
2024-08-31 17:31:45 -0300

    Created the static version of the chart showing how much kilometers I've run in 2024

--- pages/hidden/total-km-2024/chart.js
+++ pages/hidden/total-km-2024/chart.js
@@ -1,14 +1,58 @@
-export const plotChart = async () => {
-    const data = await prepareData()
+import { colours } from "../../../node_modules/visual-components/index.js"
+
+export const plotChart = async (chartProps) => {
+    const data = await prepareData(2024)
+    const { chart, width, height } = chartProps
+    const palette = colours.paletteLightBg
+
+    const x = d3
+        .scaleTime()
+        .domain(d3.extent(data, d => d.date))
+        .range([0, width])
+
+    const y = d3
+        .scaleLinear()
+        .domain([0, d3.max(data, d => d.distance) * 1.05])
+        .range([height, 0])
+
+    const area = d3
+        .area()
+        .x(d => x(d.date))
+        .y0(y(0))
+        .y1(d => y(d.distance))
+
+    chart
+        .selectAll('.area-path')
+        .data([data])
+        .join('path')
+        .attr('class', 'area-path')
+        .attr('fill', palette.blue)
+        .attr('opacity', 0.25)
+        .attr('d', area)
+
+    const line = d3
+        .line()
+        .x(d => x(d.date))
+        .y(d => y(d.distance))
+
+    chart
+        .selectAll('.line-path')
+        .data([data])
+        .join('path')
+        .attr('class', 'line-path')
+        .attr('fill', 'none')
+        .attr('stroke', palette.blue)
+        .attr('stroke-width', 1)
+        .attr('d', line)
 
     console.log(data);
     console.log(d3.max(data, d => d.distance / 1000))
 }
 
-async function prepareData() {
+async function prepareData(year = 2024) {
     const distances = await d3.csv('/data/activities.csv')
         .then(dt => dt
-            .filter(d => new Date(d.local_time).getFullYear() === 2024)
+            .filter(d => new Date(d.local_time).getFullYear() === year)
             .map(d => {
                 const localDate = new Date(d.local_time)
                 return {
@@ -20,7 +64,7 @@ async function prepareData() {
 
     const data = []
 
-    const currentDate = new Date('2024-01-01T00:00:00Z')
+    const currentDate = new Date(`${year}-01-01T00:00:00Z`)
     for (let i = 0; i < 366; i++) {
         const dateKey = currentDate.toISOString().substring(0, 10)
 
--- pages/hidden/total-km-2024/script.js
+++ pages/hidden/total-km-2024/script.js
@@ -1,7 +1,7 @@
-import { appendChartContainer } from "../../../node_modules/visual-components/index.js"
+import { appendChartContainer, getChart } from "../../../node_modules/visual-components/index.js"
 import { plotChart } from "./chart.js"
 
-appendChartContainer({ idNum: 1, chartTitle: 'Total Km 2024' })
+const totalKmId = appendChartContainer({ idNum: 1, chartTitle: 'Total Km 2024' })
+await new Promise(r => setTimeout(r, 1));
 
-
-plotChart()
+plotChart(getChart({ id: totalKmId }))
